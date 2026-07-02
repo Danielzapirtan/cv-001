8533b84c69e5930016efb01347e8a019e569827f
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-03 00:00:39 +0300

    Fri Jul  3 00:00:39 EEST 2026

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const date = document.getElementById("date");
-const form = document.getElementById("form");
+const formular = document.getElementById("form");
 const aplica = document.getElementById("aplica");
 const timetable = document.getElementById("timetable");
 
-form.addEventListener("submit", function(e) => {
+formular.addEventListener("submit", function(e) => {
   e.preventDefault();
   const dateValue = date.value;
   const nameValue = document.getElementById("name").value;
   const descValue = document.getElementById("desc").value || "";
   const startValue = document.getElementById("start").value;
   const endValue = document.getElementById("end").value;
-  const record = document.createElement("p");
-  record.innerHTML = `${dateValue} ${nameValue}
+  const inregistrare = document.createElement("p");
+  inregistrare.innerHTML = `${dateValue} ${nameValue} \
   ${descValue} ${startValue} ${endValue}`;
-  timetable.appendChild(record);
+  timetable.appendChild(inregistrare);
 });
