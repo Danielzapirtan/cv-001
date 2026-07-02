bbbad479b73f8473d8547857c9a7cfacfbb7a572
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2026-07-03 00:13:02 +0300

    Add dynamic paragraph creation on form submit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const date = document.getElementById("date");
 const formular = document.getElementById("form");
 const aplica = document.getElementById("aplica");
 const timetable = document.getElementById("timetable");
-const inregistrare = document.createElement("p");
 
 formular.addEventListener("submit", function(e) => {
   e.preventDefault();
@@ -11,6 +10,7 @@ formular.addEventListener("submit", function(e) => {
   const descValue = document.getElementById("desc").value || "";
   const startValue = document.getElementById("start").value;
   const endValue = document.getElementById("end").value;
+  const inregistrare = document.createElement("p");
   inregistrare.innerHTML = `${dateValue} ${nameValue} \
   ${descValue} ${startValue} ${endValue}`;
   timetable.appendChild(inregistrare);
