cdc5529b2adedf63b811b1294d2097a3dac7b2fe
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-02 14:34:57 +0300

    Thu Jul  2 14:34:57 EEST 2026

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const body = document.getElementByTagName("body");
 
+class DB {
+	init() {
+		__db = [];
+	}
+	addRecord(record) {
+		__db.push(record);
+	}
+	delRecord(record) {
+		// TODO
+	}
+	sort() {
+		__db.sort();
+	}
+	filterByDate(date) {
+		// TODO
+	}
+}
+
+class Timetable {
+	init(db, date) {
+		db.filterByDate(date);
+	}
+	display() {
+		// TODO
+	}
+}
+
 function crudActivity() { }
 function handleLocalStorage() { }
+function initAll() { }
+function initDB() { }
 function pickDate() { }
 function populateElements() { }
-function viewCalendar() { }
 
