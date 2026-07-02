381844edd3e5fcb736a74c279b84b308e680072d
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-02 16:35:52 +0300

    Thu Jul  2 16:35:52 EEST 2026

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 class ActivityWidget { }
-class AppWindow { }
+class AppWindow {
+  constructor init() {
+    const body = document.getElementByTagName("body");
+    const appWindow = document.createElement("div");
+    appWindow.classList.add("app-window");
+    body.appendChild(appWindow);
+    appWindow.innerHTML = `Hello cv-001`;
+  }
+}
+
 class DatePickerWidget { }
 class MainWidget { }
 class TimetableWidget { }
+
+const myAppWindow = new AppWindow();
